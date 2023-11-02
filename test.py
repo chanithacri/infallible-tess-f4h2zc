d604d7f4666d3da53fb0f2c16c1f43e18cf64090
chanithacri <113086455+chanithacri@users.noreply.github.com>
2023-11-02 13:51:17 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-    def num_stops_per_bus(self):
-        buses = json.loads(str(input()))
-        dict_ = dict()
-        for data in buses:
-            dict_.setdefault(data["bus_id"], 0)
-            dict_[data["bus_id"]] += 1
 
-        print("Line names and number of stops")
-        for key, value in dict_.items():
-            print("bus_id: {}, stops: {}".format(key, value))
